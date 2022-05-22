c3824e0c7496bad914e37cbe0e1bd922f7e3065e
Thiolk <88479051+Thiolk@users.noreply.github.com>
2022-05-22 23:03:01 +0800

    Level 2-2

---
+++ Shooting Portals/Assets/Scripts/WaypointFollowerEnemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollowerEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+    [SerializeField] private float speed = 2f;
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f) 
+        {
+           currentWaypointIndex ++;
+           if (currentWaypointIndex >= waypoints.Length)
+           {
+               currentWaypointIndex = 0;
+           }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+        if (currentWaypointIndex == 0) 
+        {
+            transform.localScale = new Vector2(-1,1);
+        }
+        else 
+        {
+            transform.localScale = new Vector2(1,1);
+        }
+    }
+}
