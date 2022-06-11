2f93aca7040f17bd8031ebe5e3fe1271164a25df
Eugene Tang <dragoneugene10@gmail.com>
2022-06-11 16:37:41 +0800

    Updated tutorial in level 2

--- Shooting Portals/Assets/Scripts/LevelData/LevelOne.cs
+++ Shooting Portals/Assets/Scripts/LevelData/LevelOne.cs
@@ -6,7 +6,7 @@ public class LevelOne : LevelData
 {
     private float[,] spawnPositions = new float[,]
         {
-            {46.682f, -6.426f}, //Checkpoint 1 {-9.542f, 2.519f} {47.72f, -6.435f}, 
+            {-9.542f, 2.519f}, //Checkpoint 1 {-9.542f, 2.519f} {47.72f, -6.435f}, 
             {16.52484f, 4.135397f}, //Checkpoint 2
             {42.001f, 0.578f} //Checkpoint 3
         };
--- Shooting Portals/Assets/Scripts/LevelData/LevelTwo.cs
+++ Shooting Portals/Assets/Scripts/LevelData/LevelTwo.cs
@@ -6,7 +6,7 @@ public class LevelTwo : LevelData
 {
     private float[,] spawnPositions = new float[,]
         {
-            {-29.92f, 0.57f}, //Checkpoint 1 {-29.92f, 0.57f}
+            {30.295f, -5.43f}, //Checkpoint 1 {-29.92f, 0.57f}
             {-7.68f, 0.55f}, //Checkpoint 2
             {15.35f, 0.5729118f}, //Checkpoint 3
             {30.295f, -5.43f}, //Checkpoint 4
