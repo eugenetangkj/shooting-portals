3b3a0f6ad1837352362f959b57cd8dd5680693a9
Eugene Tang <dragoneugene10@gmail.com>
2022-06-13 16:03:54 +0800

    Started on rooms 1 and 2 of level 6

---
+++ Shooting Portals/Assets/Scripts/LevelData/LevelSix.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSix : LevelData
+{
+    private float[,] spawnPositions = new float[,]
+        {
+            {-36.22847f, -21.46f}, //Checkpoint 1  {-36.22847, -21.46f}
+            {16.13479f, 0.5729172f}, //Checkpoint 2
+            {41.19f, 7.56f}, //Checkpoint 3
+            {47f, -5.41f}, //Checkpoint 4
+            {69.75368f, -7.424f} //Checkpoint 5
+        };
+
+    public override float[,] getSpawnPositions()
+    {
+        return spawnPositions;
+    }
+}
