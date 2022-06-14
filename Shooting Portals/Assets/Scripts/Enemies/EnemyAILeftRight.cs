9d1d6d78618bf49cace3e92649085bbdd1f72039
Eugene Tang <dragoneugene10@gmail.com>
2022-06-14 16:59:31 +0800

    Working on rooms 3 and 4

---
+++ Shooting Portals/Assets/Scripts/Enemies/EnemyAILeftRight.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAILeftRight : MonoBehaviour
+{
+    [SerializeField] private Player player;
+
+    [SerializeField] private GameObject enemyToDestroy;
+    [SerializeField] private DeathLogic deathLogic;
+    [SerializeField] private AudioSource hitSound;
+
+
+    public bool shouldReturn;
+
+    private float enemyX;
+    private Animator anim;
+    private bool isAlive;
+
+
+    private Vector3 enemyOriginalPos;
+    [SerializeField] private float speed = 0.5f;
+    // Update is called once per frame
+    
+    private void Start()
+    {
+        enemyOriginalPos = transform.position;
+        enemyX = transform.position.x;
+        anim = this.GetComponent<Animator>();
+        shouldReturn = false;
+        isAlive = true;
+    }
+
+    
+    private void Update()
+    {
+        if (isAlive)
+        {
+            if (transform.position == enemyOriginalPos)
+            {
+                anim.SetBool("move", false);
+                transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+
+            }
+            else
+            {
+            anim.SetBool("move", true);
+            }
+
+            enemyX = transform.position.x;
+
+
+            if (shouldReturn)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, enemyOriginalPos, Time.deltaTime * speed);
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, enemyOriginalPos.y), Time.deltaTime * speed);
+            }
+
+            if (transform.position.x >= enemyX)
+            {
+                //Moving towards the right
+                transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+            }
+            else 
+            {
+                //Moving towards the left
+                transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
+            }
+        }
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Attack Bullet")
+        {
+            isAlive = false;
+            enemyToDestroy.GetComponent<Animator>().SetTrigger("destroy");
+            this.hitSound.Play();
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(deathLogic);
+            Invoke("DestroyEnemy", 1.2f);
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(enemyToDestroy);
+    }
+}
--- Shooting Portals/Assets/Scripts/LevelData/LevelSix.cs
+++ Shooting Portals/Assets/Scripts/LevelData/LevelSix.cs
@@ -6,10 +6,10 @@ public class LevelSix : LevelData
 {
     private float[,] spawnPositions = new float[,]
         {
-            {-36.22847f, -21.46f}, //Checkpoint 1  {-36.22847, -21.46f}
+            {-36.22847f, -21.46f}, //Checkpoint 1  {-36.22847f, -21.46f}
             {-7.16f, -19.4271f}, //Checkpoint 2
             {17.8f, -16.42711f}, //Checkpoint 3
-            {47f, -5.41f}, //Checkpoint 4
+            {39.5f, -22.45f}, //Checkpoint 4
             {69.75368f, -7.424f} //Checkpoint 5
         };
 
