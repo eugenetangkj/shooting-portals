12adaa40fbc2e8723169ce26b0a753d604bdb99c
Eugene Tang <dragoneugene10@gmail.com>
2022-06-14 13:56:10 +0800

    Completed room 3 of level 3 with animations

--- Shooting Portals/Assets/Scripts/LevelData/LevelSix.cs
+++ Shooting Portals/Assets/Scripts/LevelData/LevelSix.cs
@@ -7,8 +7,8 @@ public class LevelSix : LevelData
     private float[,] spawnPositions = new float[,]
         {
             {-36.22847f, -21.46f}, //Checkpoint 1  {-36.22847, -21.46f}
-            {16.13479f, 0.5729172f}, //Checkpoint 2
-            {41.19f, 7.56f}, //Checkpoint 3
+            {-7.16f, -19.4271f}, //Checkpoint 2
+            {17.8f, -16.42711f}, //Checkpoint 3
             {47f, -5.41f}, //Checkpoint 4
             {69.75368f, -7.424f} //Checkpoint 5
         };
