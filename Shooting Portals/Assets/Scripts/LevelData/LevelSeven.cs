1a4823957b1084c1fbd0004f41a42dd53f278037
Eugene Tang <dragoneugene10@gmail.com>
2022-06-16 12:28:04 +0800

    Completed level 7 with audio

--- Shooting Portals/Assets/Scripts/LevelData/LevelSeven.cs
+++ Shooting Portals/Assets/Scripts/LevelData/LevelSeven.cs
@@ -11,6 +11,8 @@ public class LevelSeven : LevelData
             {16.51f, -40.44f}, //Checkpoint 3
             {-6.39f, -54.42f}, //Checkpoint 4
             {17.46f, -54.42f}, //Checkpoint 5
+            {43.57f, -54.42f}, //Checkpoint 6
+            {55.067f, -66.429f} //Checkpoint 7
         };
 
     public override float[,] getSpawnPositions()
