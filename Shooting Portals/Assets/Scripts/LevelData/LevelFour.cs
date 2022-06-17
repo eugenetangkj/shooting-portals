3541b86c131a61e8ca4a6a434c94df546efa7ddd
Eugene Tang <dragoneugene10@gmail.com>
2022-06-18 00:33:28 +0800

    Completed till room 8 of level 3

--- Shooting Portals/Assets/Scripts/LevelData/LevelFour.cs
+++ Shooting Portals/Assets/Scripts/LevelData/LevelFour.cs
@@ -6,10 +6,12 @@ public class LevelFour : LevelData
 {
     private float[,] spawnPositions = new float[,]
         {
-            {172.6218f, -24.42709f}, //Checkpoint 1 {96.49f, -24.45f}
+            {96.49f, -24.45f}, //Checkpoint 1 {96.49f, -24.45f}
             {121.1546f, -24.45f}, //Checkpoint 2
             {153.059f, -24.413f}, //Checkpoint 3
-            {172.6218f, -24.42709f} //Checkpoint 4
+            {172.6218f, -24.42709f}, //Checkpoint 4
+            {198.54f, -17.4f}, //Checkpoint 5
+            {225.5393f, -24.421f} //Checkpoint 6
         };
 
     public override float[,] getSpawnPositions()
