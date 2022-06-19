8ab0039a1a77179a820a4ffee0eb1d130686786d
Eugene Tang <dragoneugene10@gmail.com>
2022-06-19 11:45:03 +0800

    Changed back starting coordinates for lvl 4

--- Shooting Portals/Assets/Scripts/LevelData/LevelFour.cs
+++ Shooting Portals/Assets/Scripts/LevelData/LevelFour.cs
@@ -6,7 +6,7 @@ public class LevelFour : LevelData
 {
     private float[,] spawnPositions = new float[,]
         {
-            {225.5393f, -24.421f}, //Checkpoint 1 {96.49f, -24.45f}
+            {96.49f, -24.45f}, //Checkpoint 1 {96.49f, -24.45f}
             {121.1546f, -24.45f}, //Checkpoint 2
             {153.059f, -24.413f}, //Checkpoint 3
             {172.6218f, -24.42709f}, //Checkpoint 4
