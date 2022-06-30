1fcb2836bea8e0bdef031379b68b08a7948c27b0
Eugene Tang <dragoneugene10@gmail.com>
2022-06-30 16:35:17 +0800

    Completed level 9 with audio

--- Shooting Portals/Assets/Scripts/Movables/Orb.cs
+++ Shooting Portals/Assets/Scripts/Movables/Orb.cs
@@ -16,7 +16,7 @@ public class Orb : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && ! hasTouched)
         {
             audioSound.Play();
             hasTouched = true;
